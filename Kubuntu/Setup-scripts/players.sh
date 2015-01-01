325f571a9fd3d93d60cff4b60e976ffeb5ebe05c
mrspoy <denis@devvela.com>
2015-01-01 18:30:53 +0500

    [WINDOWS] Added timezone fix for dual boot

--- Kubuntu/Setup-scripts/players.sh
+++ Kubuntu/Setup-scripts/players.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo add-apt-repository ppa:rvm/smplayer 
-sudo apt-get update 
+sudo apt-get update
 sudo apt-get install smplayer smtube smplayer-themes smplayer-skins -y
+
+sudo add-apt-repository ppa:banshee-team/ppa
+sudo apt-get update
+sudo apt-get install banshee -y
